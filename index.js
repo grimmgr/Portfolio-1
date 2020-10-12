b47f045513e8db9382c1acf554999c883cee50fe
grimmgr <ggrimm33@gmail.com>
2020-10-12 16:49:44 -0700

    portfolio

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// stop video if user has reduced motion settings
 const vid = document.getElementById("backgroundVideo");
 if (window.matchMedia('(prefers-reduced-motion)').matches) {
     vid.removeAttribute("autoplay");
     vid.pause();
 }
+
+// display card on hover
+// const portfolioWrapper = document.querySelector(".portfolio-wrapper");
+// const sevenTitle = document.querySelector(".title-wrapper");
+// sevenTitle.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     portfolioWrapper.
+// })
